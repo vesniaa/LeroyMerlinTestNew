64dd898be71f9af0dbddd80336d97f6019641ea0
vesniaa <pyssiki@yandex.ru>
2022-12-19 14:00:06 +0300

    Initial Commit

---
+++ LeroyMerlinTestNew/LeroyMerlinTestNewApp.swift
@@ -0,0 +1,17 @@
+//
+//  LeroyMerlinTestNewApp.swift
+//  LeroyMerlinTestNew
+//
+//  Created by Евгения Аникина on 19.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct LeroyMerlinTestNewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
